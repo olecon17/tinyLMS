c032fa5ef04b6f7146174f095a403bce1997d358
olecon17 <oleary.conor17@gmail.com>
2018-02-25 11:44:49 -0600

    CRUD actions working for users

---
+++ client/src/app/user-edit/user-edit.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+
+
+import { UserService } from '../shared/user/user.service';
+
+import { NgForm } from '@angular/forms';
+import {Subscription} from "rxjs/Subscription";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-user-edit',
+  templateUrl: './user-edit.component.html',
+  styleUrls: ['./user-edit.component.css']
+})
+export class UserEditComponent implements OnInit, OnDestroy {
+
+  user: any = {};
+
+  sub: Subscription;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private userService: UserService) { }
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      console.log(params);
+      const id = params['id'];
+      if (id) {
+        this.userService.get(id).subscribe((user: any) => {
+          if (user) {
+            console.log(user);
+            this.user = user;
+          } else {
+            console.log(`User with id '${id}' not found, returning to list`);
+            this.gotoList();
+          }
+        });
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  gotoList() {
+    this.router.navigate(['/user-list']);
+  }
+
+  save(form: NgForm) {
+    console.log(form);
+    this.userService.save(form).subscribe(result => {
+      this.gotoList();
+    }, error => console.error(error))
+  }
+
+  remove(id: string) {
+    this.userService.remove(id).subscribe(result => {
+      this.gotoList();
+    }, error => console.error(error))
+  }
+
+
+
+}
